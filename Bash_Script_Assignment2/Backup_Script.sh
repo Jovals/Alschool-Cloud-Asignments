5dc211e2c6834d196259067fa0bb58c3a0e8348b
jovals <josephatkene@gmail.com>
2024-03-15 16:01:52 +0100

    modified backup script

--- Bash_Script_Assignment2/Backup_Script.sh
+++ Bash_Script_Assignment2/Backup_Script.sh
@@ -1,13 +1,34 @@
-#!/bin/bash
-#sudo apt update
-#sudo apt install tar
 
-timestamp=$(date '+%Y_%m_%d_%H_%M_%S')
+#!/bin/bash
+
+
+timestamp=$(date '+%Y_%m_%d_%H-%M-%S')
 source_directory=$1
 destination_directory=$2
 backup_directory=$destination_directory$timestamp.tar.gz 
 
+if [ $# -ne 2 ]
+then
+  echo "Usage: backup.sh <source_directory> <target_directory>"
+  echo "Please try again"
+  exit 1 
+fi
+
+if [ ! -d "$source_directory" ]; then
+	echo "Source directory $source_directory does not exist"
+	exit 2
+fi
+
+if [ -z "$(ls -A $source_dirrctory)" ]; then
+	echo "Source dir $source_diretory is empty"
+	exit 3
+fi
+
+if [ ! -d "$destination_directory" ]; then
+	mkdir -p $destination_directory
+fi
 
 sudo tar -czf $backup_directory  $source_directory
 
 echo $backup_directory
+
